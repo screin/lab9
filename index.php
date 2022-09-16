44ff0f822f42567b3704d280f2b2131371fb5820
Attila Papai <screin@stargate.screinhome.nz>
2022-09-16 17:16:30 +1200

    4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body> 
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
-		<?php echo "CSAAAAAAAAAAAAAAAAAAAAAAA VILAG!!!!!!!!
+		<?php echo "CSAAAAAAAAAAAAAAAAAAAAAAA VILAG!!!!!!!!";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> </p> 
