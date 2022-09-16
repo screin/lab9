1cd2e99b952839819a1898a728f7eba5d7f5bc3c
Attila Papai <screin@stargate.screinhome.nz>
2022-09-16 16:34:34 +1200

    3

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 <body> 
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
-		<?php echo "The Current Date and Time is: <br />"; 
+		<?php echo "CSAAAAAAAAAAAAAAAAAAAAAAA VILAG!!!!!!!!
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
+		<p> </p> 
         <h2>GOING ON VACATION</h2>
 	</body> 
 </html>
